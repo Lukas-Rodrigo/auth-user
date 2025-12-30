b6d0e517732d9ff33a5f36f5146c2c3ac78a709e
Lukas-Rodrigo <lucasrodrigo0503@gmail.com>
2025-12-30 18:10:43 -0300

    feat: refactor JwtAuthFilter para extrair token do cookie

--- src/main/java/com/lucastexeira/authuser/infra/filter/JwtAuthFilter.java
+++ src/main/java/com/lucastexeira/authuser/infra/filter/JwtAuthFilter.java
@@ -1,16 +1,19 @@
 package com.lucastexeira.authuser.infra.filter;
 
 
+import com.lucastexeira.authuser.core.port.out.TokenServiceOutputPort;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -20,43 +23,29 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
+import java.util.Arrays;
 import java.util.List;
 
 @Component
 public class JwtAuthFilter extends OncePerRequestFilter {
 
+  private final Key secretKey;
 
+  private final TokenServiceOutputPort tokenService;
 
-  private Key secretKey;
-
-  public JwtAuthFilter(@Value("${jwt.SECRET_KEY}") String secret) {
+  public JwtAuthFilter(@Value("${jwt.SECRET_KEY}") String secret, TokenServiceOutputPort tokenService) {
     this.secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    this.tokenService = tokenService;
   }
 
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-    String authHeader = request.getHeader("Authorization");
+    String token = extractTokenFromCookie(request);
 
-    if (authHeader != null && authHeader.startsWith("Bearer ")) {
+    if (token != null && this.tokenService.isTokenValid(token)) {
         try {
-          String token = authHeader.substring(7);
-
-          Claims claims =
-              Jwts
-                  .parserBuilder()
-                  .setSigningKey(secretKey).build()
-                  .parseClaimsJws(token)
-                  .getBody();
-
-          String role = claims.get("role", String.class);
-          List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority("ROLE_" + role));
-          UsernamePasswordAuthenticationToken authentication =
-              new UsernamePasswordAuthenticationToken(
-                  claims.getSubject(),
-                  null
-                  , authorities
-              );
+          Authentication authentication = getAuthentication(token);
           SecurityContextHolder.getContext().setAuthentication(authentication);
         } catch (JwtException ex) {
           SecurityContextHolder.clearContext();
@@ -66,5 +55,37 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 
     }
     filterChain.doFilter(request, response);
+  }
+
+
+  private Authentication getAuthentication(String token) {
+    Claims claims = parseClaims(token);
+
+    String role = claims.get("role", String.class);
+    List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority("ROLE_" + role));
+
+    return new UsernamePasswordAuthenticationToken(
+        claims.getSubject(),
+        null
+        , authorities
+    );
+  }
+
+  private Claims parseClaims(String token) {
+    return Jwts.parserBuilder()
+        .setSigningKey(secretKey)
+        .build()
+        .parseClaimsJws(token)
+        .getBody();
+  }
+
+  private String extractTokenFromCookie(HttpServletRequest request) {
+    if (request.getCookies() == null) return null;
+
+    return Arrays.stream(request.getCookies())
+        .filter(cookie -> "access_token".equals(cookie.getName()))
+        .map(Cookie::getValue)
+        .findFirst()
+        .orElse(null);
   }
 }
