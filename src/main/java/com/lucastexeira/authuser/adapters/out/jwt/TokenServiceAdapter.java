7b8ed165b86eccc6362120e28b28f2527083f9d8
Lukas-Rodrigo <lucasrodrigo0503@gmail.com>
2025-12-30 18:09:14 -0300

    feat: adicionar suporte a tokens de refresh e expiração de chaves no adaptador de serviço de token

--- src/main/java/com/lucastexeira/authuser/adapters/out/jwt/TokenServiceAdapter.java
+++ src/main/java/com/lucastexeira/authuser/adapters/out/jwt/TokenServiceAdapter.java
@@ -15,8 +15,11 @@ public class TokenServiceAdapter implements TokenServiceOutputPort {
   @Value("${jwt.SECRET_KEY}")
   private String secretKey;
 
-  @Value("${jwt.EXPIRATION_TIME}")
-  private Long expirationTime;
+  @Value("${jwt.SECRET_KEY_EXPIRATION_TIME}")
+  private Long secretKeyExpirationTime;
+
+  @Value("${jwt.REFRESH_KEY_EXPIRATION_TIME}")
+  private Long refreshKeyExpirationTime;
 
 
   @Override
@@ -25,8 +28,55 @@ public class TokenServiceAdapter implements TokenServiceOutputPort {
         .setSubject(user.getId())
         .claim("role", user.getRole().name())
         .setIssuedAt(new Date())
-        .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
+        .setExpiration(new Date(System.currentTimeMillis() + secretKeyExpirationTime))
         .signWith(Keys.hmacShaKeyFor(secretKey.getBytes()))
         .compact();
+  }
+
+  @Override
+  public String generateRefreshToken(User user) {
+    return Jwts.builder()
+        .setSubject(user.getId())
+        .claim("type", "REFRESH")
+        .setIssuedAt(new Date())
+        .setExpiration(new Date(System.currentTimeMillis() + refreshKeyExpirationTime))
+        .signWith(Keys.hmacShaKeyFor(secretKey.getBytes()))
+        .compact();
+  }
+
+  @Override
+  public String getUserIdFromRefreshToken(String refreshToken) {
+    return Jwts.parserBuilder()
+        .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes()))
+        .build()
+        .parseClaimsJws(refreshToken)
+        .getBody()
+        .getSubject();
+  }
+
+  @Override
+  public boolean isRefreshTokenValid(String refreshToken) {
+    try {
+      Jwts.parserBuilder()
+          .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes()))
+          .build()
+          .parseClaimsJws(refreshToken);
+      return true;
+    } catch (Exception e) {
+      return false;
+    }
+  }
+
+  @Override
+  public boolean isTokenValid(String token) {
+    try {
+      Jwts.parserBuilder()
+          .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes()))
+          .build()
+          .parseClaimsJws(token);
+      return true;
+    } catch (Exception e) {
+      return false;
+  }
   }
 }
